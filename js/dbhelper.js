ce5a8f1fac0e2a0846e7f350753db483086b858a
Ed W <cew005@yahoo.com>
2019-01-07 20:19:27 -0600

    add offline web page if a request is not in cache and offline. Update service worker fetch from class notes/work

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://latdc04096:${port}/data/restaurants.json`;
+    return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
--- sw.js
+++ sw.js
@@ -6,37 +6,45 @@ self.addEventListener('install', function(event) {
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
-        './css/styles.css',
-        './data/restaurants.json',
-        './img/1.jpg',
-        './img/10.jpg',
-        './img/2.jpg',
-        './img/3.jpg',
-        './img/4.jpg',
-        './img/5.jpg',
-        './img/6.jpg',
-        './img/7.jpg',
-        './img/8.jpg',
-        './img/9.jpg',
-        './index.html',
-        './js/dbhelper.js',
-        './js/main.js',
-        './js/restaurant_info.js',
-        './restaurant.html'
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/10.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/index.html',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/offline.html',
+        '/restaurant.html'
       ]);
     })
   );
 });
 
 // look in the cache 1st , if a match then use the cache otherwise fetch from the internet
-self.addEventListener('fetch', (e) => {
- e.respondWith(caches.match(e.request).then((response) => {
-  if(response)
-   return response
-  else
-   return fetch(e.request)
- }) )
+self.addEventListener('fetch', function(event) {
+event.respondWith(
+caches.match(event.request)
+      .then(function(response) {
+        // If a cache is hit, we can return the response.
+        if (response) {
+          return response;
+        }
+          return fetch(event.request);
+
+}).catch(function(){
+console.log('OFFLINE Condition for a page request that has not been cached');
+return fetch('/offline.html');
 })
 
-
+);
+});
 
